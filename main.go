45cc33401a0faeb337edebef9d9baaba8109d2a4
NielsPrins <NielsPrins@users.noreply.github.com>
2024-04-22 21:49:35 +0200

    Go setup

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type Data struct {
+	Title string
+}
+
+func main() {
+	homeHandler := func(w http.ResponseWriter, req *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		data := Data{
+			"Go setup",
+		}
+
+		tmpl.Execute(w, data)
+	}
+
+	http.HandleFunc("/", homeHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
