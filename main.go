afe4bbce7fa42e6e7e69169d8eee418eb4f62a82
NielsPrins <NielsPrins@users.noreply.github.com>
2024-04-25 16:55:38 +0200

    Air live reload

--- main.go
+++ main.go
@@ -2,30 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
+	"homepage/routes/home"
 	"log"
 )
 
-type Data struct {
-	Title string
-}
-
 func main() {
-	engine := html.New("./views", ".gohtml")
-
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New()
 
 	app.Static("/public", "./public")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		data := Data{
-			Title: "Go setup",
-		}
-
-		return c.Render("index", data)
-	})
+	app.Get("/", home.Handler)
 
 	log.Fatal(app.Listen(":3000"))
 }
---
+++ routes/home/home.go
@@ -0,0 +1,18 @@
+package home
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Data struct {
+	Title string
+}
+
+func Handler(c *fiber.Ctx) error {
+
+	data := Data{
+		Title: "Go setup",
+	}
+
+	return c.Render("routes/home/home.gohtml", data)
+}
