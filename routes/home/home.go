a40d91ca6da8483f0a04c887c76c37230782a42a
NielsPrins <NielsPrins@users.noreply.github.com>
2024-04-29 20:45:31 +0200

    Embed route template

--- routes/home/home.go
+++ routes/home/home.go
@@ -1,18 +1,38 @@
 package home
 
 import (
+	"embed"
 	"github.com/gofiber/fiber/v2"
+	"html/template"
 )
+
+//go:embed home.gohtml
+var htmlTemplate embed.FS
 
 type Data struct {
 	Title string
 }
 
 func Handler(c *fiber.Ctx) error {
-
 	data := Data{
 		Title: "Go setup!!",
 	}
 
-	return c.Render("routes/home/home.gohtml", data)
+	tmplData, err := htmlTemplate.ReadFile("home.gohtml")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error reading template file")
+	}
+
+	tmpl, err := template.New("home").Parse(string(tmplData))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error parsing template")
+	}
+
+	c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+	err = tmpl.ExecuteTemplate(c, "home", data)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error executing template")
+	}
+
+	return nil
 }
